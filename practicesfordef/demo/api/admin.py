b7fac001348077d77ac6608245336dafc35055b1
aknuraru <ryskulaknur243@gmail.com>
2026-04-09 09:25:02 +0500

    Delete practicesfordef directory

--- practicesfordef/demo/api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Movie
-# Register your models here.
-@admin.register(Movie)
-class MovieAdmin(admin.ModelAdmin):
-    list_display = ('id','title', 'director','year','rating')
-    search_fields = ('title', 'genre')
--- practicesfordef/movie_project/api/urls.py
+++
@@ -1,6 +0,0 @@
-from . import views
-from django.urls import path
-urlpatterns = [
-    path('movies/',views.list),
-    path('movies/<int:id>/',views.detail),
-]
